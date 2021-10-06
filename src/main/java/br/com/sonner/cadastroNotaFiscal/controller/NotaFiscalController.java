de2541a8e99c67bd03eb73a4dd4982ddb8a2065f
neftalicris <cristina@sonner.com.br>
2021-10-06 17:01:13 -0300

    Notas atualização

--- src/main/java/br/com/sonner/cadastroNotaFiscal/controller/NotaFiscalController.java
+++ src/main/java/br/com/sonner/cadastroNotaFiscal/controller/NotaFiscalController.java
@@ -5,9 +5,11 @@ import br.com.sonner.cadastroNotaFiscal.DTO.NotaFiscalDTO;
 import br.com.sonner.cadastroNotaFiscal.controller.form.ClienteFormDTO;
 import br.com.sonner.cadastroNotaFiscal.controller.form.NotaFiscalFormDTO;
 import br.com.sonner.cadastroNotaFiscal.model.Cliente;
+import br.com.sonner.cadastroNotaFiscal.model.ItensNota;
 import br.com.sonner.cadastroNotaFiscal.model.NotaFiscal;
 import br.com.sonner.cadastroNotaFiscal.model.Produto;
 import br.com.sonner.cadastroNotaFiscal.repository.ClienteRepository;
+import br.com.sonner.cadastroNotaFiscal.repository.ItensNotaRepository;
 import br.com.sonner.cadastroNotaFiscal.repository.NotaFiscalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,15 +29,27 @@ public class NotaFiscalController {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private ItensNotaRepository itensNotaRepository;
+
+    private  ItensNotaController itensNotaController;
+
     @GetMapping
     public List<NotaFiscalDTO> lista() {
         List<NotaFiscal> notaFiscal = (List<NotaFiscal>) notaFiscalRepository.findAll();
         return NotaFiscalDTO.lista(notaFiscal);
 
     }
+//    @PostMapping("/")
+//    public NotaFiscal salvar(@RequestBody NotaFiscal notaFiscal){
+//        notaFiscal = notaFiscalRepository.save(notaFiscal);
+//        return notaFiscal;
+//    }
     @PostMapping("/")
-    public NotaFiscal salvar(@RequestBody NotaFiscal notaFiscal){
+    public NotaFiscal salvar(@RequestBody NotaFiscal notaFiscal, ItensNota itensNota){
         notaFiscal = notaFiscalRepository.save(notaFiscal);
+        itensNota = itensNotaRepository.save(itensNota);
+        itensNotaController.salvar(itensNota);
         return notaFiscal;
     }
 
